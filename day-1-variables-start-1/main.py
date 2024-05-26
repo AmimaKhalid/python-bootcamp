8b2715f42e5eea88b97d6da1d1fe8a82742bdd8a
Amima Khalid <amimakhalid27@gmail.com>
2024-05-26 17:18:54 +0500

    Amusing with python .1

---
+++ day-1-variables-start-1/main.py
@@ -0,0 +1,16 @@
+#print("Hello World!\nHello World!\nHello World")
+#print("Hello "+" "+" Amima")
+#input("what is your name?")
+#print("Hello " +  input("what is your name?"))
+name = "Amima"
+print(name)
+name ="Moiz"
+print(name)
+name = input("what is your name?")
+length = len(name)
+print(length)
+
+
+user_name = input("what is your name?")
+length = len(user_name)
+print(length)
